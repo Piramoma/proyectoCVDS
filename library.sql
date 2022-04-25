2acc73ca21b864aba9814c3380af0b66e5e2d37d
Yesid Mora <yesid.mora@mail.escuelaing.edu.co>
2022-04-25 12:16:08 -0500

    Actualizacion base de datos

--- library.sql
+++ library.sql
@@ -1,44 +1,51 @@
-CREATE SEQUENCE idRecurso INCREMENT BY 1 START WITH 1;
-
 CREATE TABLE disponibilidad (
-   id int4 NOT NULL,
+   id serial NOT NULL,
    idrecurso int4 NOT NULL,
    fechainicio timestamp NOT NULL,
    fechafin timestamp NOT NULL
 );
 
 CREATE TABLE recursos (
-     id int4 AUTO_INCREMENT NOT NULL,
+     id serial NOT NULL,
+  	 idInterno int4 NOT NULL,
      estado varchar(100) NOT NULL,
      nombre varchar(100) NOT NULL,
      ubicacion varchar(100) NOT NULL,
      tipo varchar(100) NOT NULL,
-     capacidad int4 NOT NULL,
-     horainicio timestamp NULL,
-     horafin timestamp NULL
+     capacidad int4 NOT NULL
 );
 
 CREATE TABLE reservas (
-     id int4 NOT NULL,
+     id serial NOT NULL,
      id_usuario varchar(200) NULL,
      id_recurso int4 NULL,
      fecha_inicio_reserva timestamp NULL,
      fecha_fin_reserva timestamp NULL,
      fecha_solicitud date NULL,
-     tipo varchar NULL,
      activa bool NULL
 );
+
 CREATE TABLE usuarios (
     email varchar(200) NOT NULL,
     nombre varchar(100) NOT NULL,
     apellido varchar(100) NOT NULL,
+    programa varchar(100) NOT NULL,
     contraseña varchar(32) NOT NULL,
     tipo varchar(32) NULL
 );
 
 ALTER TABLE usuarios add PRIMARY KEY (email);
-ALTER TABLE recursos add PRIMARY KEY (id);
-ALTER TABLE reservas add PRIMARY KEY (id);
-ALTER TABLE disponibilidad add PRIMARY KEY (id);
+//ALTER TABLE recursos add PRIMARY KEY (id);
+//ALTER TABLE reservas add PRIMARY KEY (id);
+//ALTER TABLE disponibilidad add PRIMARY KEY (id);
 
-insert into Recursos(estado, nombre, ubicacion, tipo, capacidad, horainicio, horafin) values ('disponible','El quijote de la mancha', 'Bloque A', 'libro', 2, '2022-06-22 19:10:25-07', '2022-06-23 19:10:25-07');
+ALTER TABLE RECURSOS ADD CONSTRAINT UK_RECURSOS_idInterno UNIQUE (idInterno);
+
+ALTER TABLE RESERVAS ADD CONSTRAINT FK_RESERVAS_USUARIOS FOREIGN KEY ( id_usuario )
+REFERENCES USUARIOS ( email );
+ALTER TABLE RESERVAS ADD CONSTRAINT FK_RESERVAS_RECURSOS FOREIGN KEY ( id_recurso )
+REFERENCES RECURSOS ( id );
+ALTER TABLE DISPONIBILIDAD ADD CONSTRAINT FK_DISPONIBILIDAD_idRecurso FOREIGN KEY ( idrecurso )
+REFERENCES RECURSOS ( id );
+
+
